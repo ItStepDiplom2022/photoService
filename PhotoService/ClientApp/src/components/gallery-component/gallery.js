7626f100a9687f586c47256cfd9d071cc0d8a555
Vitaliy <82705740+N0Test@users.noreply.github.com>
2022-07-15 15:58:30 +0300

    created recomendation list front

---
+++ PhotoService/ClientApp/src/components/gallery-component/gallery.js
@@ -0,0 +1,45 @@
+import "./gallery.css";
+import React from "react";
+import GalleryItem from "./gallery-item-component/gallery-item";
+
+export default function Gallery(props) {
+  var getVal = function (elem, style) {
+    return parseInt(window.getComputedStyle(elem).getPropertyValue(style));
+  };
+
+  var getHeight = function (item) {
+    return item.querySelector(".content").getBoundingClientRect().height;
+  };
+
+  var resizeAll = function () {
+    var gallery = document.querySelector("#gallery");
+    var altura = getVal(gallery, "grid-auto-rows");
+    var gap = getVal(gallery, "grid-row-gap");
+    gallery.querySelectorAll(".gallery-item").forEach(function (item) {
+      var el = item;
+      el.style.gridRowEnd =
+        "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
+    });
+  };
+  window.addEventListener("resize", resizeAll);
+
+  function resizeItem(item) {
+    if (item.complete) {
+        var gallery = document.querySelector("#gallery");
+        var altura = getVal(gallery, "grid-auto-rows");
+        var gap = getVal(gallery, "grid-row-gap");
+        var gitem = item.parentElement.parentElement;
+        gitem.style.gridRowEnd =
+          "span " + Math.ceil((getHeight(gitem) + gap) / (altura + gap));
+        item.parentElement.classList.remove("byebye");
+    }
+  }
+
+  return (
+    <div class="gallery" id="gallery">
+      {props.images.map(function (image) {
+        return <GalleryItem image={image} resizing={resizeItem} />;
+      })}
+    </div>
+  );
+}
